1ce9c0304644c15bd91ed9da61a8d0c2fc96b4ef
Aneesha030 <167295180+Aneesha030@users.noreply.github.com>
2024-04-22 17:59:25 +0530

    source code

--- main.cpp
+++ main.cpp
@@ -11,9 +11,16 @@ float round(float var)
     return (float)value / 100;
 }
 
+bool check_number(char str) {
+    if (isdigit(str) == false){
+        return false;
+    }
+    return true;
+}
+
 class Student {
 private:
-    static const int MAX_STUDENTS = 100; // Maximum number of students
+    static const int MAX_STUDENTS = 100;
     int rollno[MAX_STUDENTS];
     int age[MAX_STUDENTS];
     string name[MAX_STUDENTS];
@@ -43,14 +50,14 @@ public:
             }
             numOfStudents++;
         } else {
-            cout << "Maximum limit of students reached! Cannot add more." << endl;
+            cout << "\t\tMaximum limit of students reached! Cannot add more." << endl;
         }
     }
 
     int checkrollnum(int rno){
         for(int i=0; i < numOfStudents ; i++){
             if(rno == rollno[i]){
-                cout<<"Roll number already exists. "<<endl;
+                cout<<"\t\tRoll number already exists. "<<endl;
                 return 1;
             }
         }
@@ -58,19 +65,19 @@ public:
     }
 
     void displayStudent(int index) {
-        cout << "Roll No : " << rollno[index] << endl;
-        cout << "Name : " << name[index] << endl;
-        cout << "Age : " << age[index] << endl;
-        cout<<"CGPA : "  << fixed << setprecision(2) << round(cgpa[index])<<endl;
+        cout << "\t\tRoll No : " << rollno[index] << endl;
+        cout << "\t\tName : " << name[index] << endl;
+        cout << "\t\tAge : " << age[index] << endl;
+        cout<<"\t\tCGPA : "  << fixed << setprecision(2) << round(cgpa[index])<<endl;
     }
 
     int searchStudent(int searchRollno) {
         for (int i = 0; i < numOfStudents; i++) {
             if (rollno[i] == searchRollno) {
-                return i; // Return index if found
+                return i; 
             }
         }
-        return -1; // Return -1 if not found
+        return -1; 
     }
 
     void updateStudent(int index, int choice) {
@@ -86,7 +93,7 @@ public:
                     }
                 }
                 if(n < numOfStudents){
-                    cout<<"This Roll Number already exists. Cannot be reassigned"<<endl;
+                    cout<<"\t\tThis Roll Number already exists. Cannot be reassigned"<<endl;
                     break;
                 }
                 rollno[index] = rno;
@@ -119,17 +126,6 @@ public:
         }
     }
 
-    void deleteStudent(int index) {
-        for (int i = index; i < numOfStudents - 1; i++) {
-            rollno[i] = rollno[i + 1];
-            name[i] = name[i + 1];
-            age[i] = age[i + 1];
-            cgpa[i] = cgpa[i+1];
-        }
-        numOfStudents--;
-        cout << "\t\tStudent Removed Successfully" << endl;
-    }
-
     void displayAllStudents() {
         if (numOfStudents == 0) {
             cout << "\t\tNo Students registered!" << endl;
@@ -142,72 +138,52 @@ public:
     }
 
     void display9plist(){
-        cout<<"\t\t\t---------9 POINTERS LIST----------"<<endl;
+        cout<<"\t\t---------9 POINTERS LIST----------"<<endl;
         for(int i=0 ; i < ncgpa ; i++)
         {
-            cout<<i+1<<". "<<ninames[i]<<"\t\t\t\t\t\t"<<fixed<<setprecision(2)<<round(nicgpa[i])<<endl;
+            cout<<"\t\t"<<i+1<<". "<<ninames[i]<<"\t\t\t\t\t\t"<<fixed<<setprecision(2)<<round(nicgpa[i])<<endl;
         }
     }
 
 };
 
-void splitString(const string& input, char delimiter, string arr[], int size, int& index)
-{
-    istringstream stream(input);
-    string token;
-    while (getline(stream, token, delimiter) && index < size)
-    {
-        arr[index++] = token;
-    }
-}
-
 class stdteachsch
 {
-private:
-    string name[30];
-    string slots[30][25];
+    string teaname[30];
+    string slot[30][25];
+    int num_slots[30];
     string stdslots[25];
-    int num_slots;
+    int numstdslots;
     static int numofteachers;
 
 public:
-    void setdata(string s, char arr[], int num)
+    void setteaslots(string nam, char arr[], int num)
     {
-        name[numofteachers] = s;
-        num_slots = num;
+        teaname[numofteachers] = nam;
+        num_slots[numofteachers] = num;
         for (int i = 0; i < num; i++)
         {
-            if (i < 25)
-                slots[numofteachers][i] = arr[i];
-            else
-            {
-                cout << "\t\tThe slots shouldn't be more than 25" << endl;
-                break;
-            }
+            slot[numofteachers][i] = arr[i];
         }
         numofteachers++;
     }
 
-    stdteachsch() {}
-
-    void getslots()
+    void getteachslots()
     {
-        string name1;
+        string tchname;
         string times;
-        char dl = ',';
-        int idx = 0;
-        int nslot = 0;
+        int n = 1;
         cout << "\t\tEnter your name: ";
-        cin >> name1;
+        cin >> tchname;
         for (int i = 0; i < numofteachers; i++)
         {
-            if (name[i] == name1)
+            if (teaname[i] == tchname)
             {
-                cout << "\t\tYou are already registered!" << endl;
+                cout << "\n\t\tYou are already registered!";
                 return;
             }
         }
-        name[numofteachers]=name1;
+        teaname[numofteachers] = tchname;
         cout << "\t\t------------------------Slots-----------------------" << endl;
         cout << "\t\t             9-10    10-11    11-12    1-2    2-3   " << endl;
         cout << "\t\tMONDAY        A        B        C       D      E   " << endl;
@@ -218,31 +194,35 @@ public:
 
         cout << "\t\tEnter your free slots in the form (A,B,C...): ";
         cin >> times;
-        int len = times.length();
-        for (int i = 0; i < len; i++)
+        for (int i = 0; i < times.length(); i++)
         {
-            if (times[i] == dl)
+            if (times[i] == ',')
             {
-                nslot++;
+                n++;
             }
         }
-        num_slots = nslot;
-        if (len > 25)
-        {
-            cout << "\t\tNumber of slots should not exceed 25" << endl;
-            return;
-        }
+        num_slots[numofteachers] = n;
 
-        splitString(times, dl, slots[numofteachers], num_slots, idx);
+        int slotIndex = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (times[slotIndex] != ',')
+            {
+                slot[numofteachers][i] = times[slotIndex++];
+            }
+            else
+            {
+                slotIndex++;
+                i--;
+            }
+        }
         numofteachers++;
     }
 
-    int nstdslots = 1;
     void getstdslots()
     {
-        string times;
-        char dl = ',';
-        int index = 0;
+        string stdslo;
+        int n = 1;
         cout << "\t\t------------------------Slots-----------------------" << endl;
         cout << "\t\t             9-10    10-11    11-12    1-2    2-3   " << endl;
         cout << "\t\tMONDAY        A        B        C       D      E   " << endl;
@@ -252,102 +232,101 @@ public:
         cout << "\t\tFRIDAY        U        V        W       X      Y   " << endl;
 
         cout << "\t\tEnter your free slots in the form (A,B,C...): ";
-        cin >> times;
-        int len = times.length();
-        for (int i = 0; i < len; i++)
+        cin >> stdslo;
+
+        for (int i = 0; i < stdslo.length(); i++)
         {
-            if (times[i] == dl)
+            if (stdslo[i] == ',')
             {
-                nstdslots++;
+                n++;
             }
         }
-        if (len > 25)
+        numstdslots = n;
+        int slotindex = 0;
+        for (int i = 0; i < numstdslots; i++)
         {
-            cout << "\t\tNumber of slots should not exceed 25" << endl;
-            return;
+            if (stdslo[slotindex] != ',')
+            {
+                stdslots[i] = stdslo[slotindex++];
+            }
+            else
+            {
+                slotindex++;
+                i--;
+            }
         }
-
-        splitString(times, dl, stdslots, 25, index);
     }
 
     void dispcommon()
     {
-        int ncm = 0;
-        string tname;
-        int ind = -1; // Initialize with -1
-        int ncmslots = 0;
-        string opti;
-        cout << "\t\tThe registered teachers are:" << endl;
-        for (int i = 0; i < numofteachers; i++)
+        string teacher;
+        cout<<"\t\tThe available teachers :"<<endl;
+        for(int i = 0 ; i < numofteachers ; i++)
         {
-            cout << "\t\t" << i + 1 << ". " << name[i] << endl;
+            cout<<teaname[i]<<endl;
         }
-        cout << "\t\tGive the teacher's name you want to check common slots with: ";
-        cin >> tname;
-
+        cout << "\t\tEnter the teacher's name you want to check with: ";
+        cin >> teacher;
+        int ind = -1;
         for (int i = 0; i < numofteachers; i++)
         {
-            if (name[i] == tname)
+            if (teaname[i] == teacher)
             {
                 ind = i;
                 break;
             }
         }
-        if (ind != -1)
+        if (ind == -1)
         {
-            for (int i = 0; i < num_slots; i++)
+            cout << "\t\tTeacher not found!" << endl;
+            return;
+        }
+
+        string cmnslots[25];
+        int n = 0;
+        for (int i = 0; i < num_slots[ind]; i++)
+        {
+            for (int j = 0; j < numstdslots; j++)
             {
-                for (int j = 0; j < nstdslots; j++)
+                if (slot[ind][i] == stdslots[j])
                 {
-                    if (slots[ind][i] == stdslots[j])
-                    {
-                        cout << "\t\tThe common slot is/are: " << stdslots[j] << endl;
-                        ncmslots++;
-                    }
-                }
-            }
-            if (ncmslots == 0)
-            {
-                cout << "\t\tThere are no common slots with you and the respective teacher." << endl;
-                cout << "\t\tWould you still like to check the teacher's free slots? [Yes/No] ";
-                cin >> opti;
-                if (opti[0] == 'y' || opti[0] == 'Y')
-                {
-                    cout << "\t\tFor the teacher: " << name[ind] << endl;
-                    cout << "\t\tThe free slots are: ";
-                    for (int i = 0; i < num_slots; i++)
-                    {
-                        if (!slots[ind][i].empty())
-                            cout << slots[ind][i] << " ";
-                    }
-                    cout << endl;
+                    cmnslots[n++] = stdslots[j];
                 }
             }
         }
-        else
+
+        if (n == 0)
         {
-            cout << "\t\tTeacher not registered. Please request the teacher to register on this application!" << endl;
+            cout << "\t\tNo common slots found." << endl;
+            return;
         }
+
+        cout << "\t\tThe common slots are: ";
+        for (int i = 0; i < n; i++)
+        {
+            cout << cmnslots[i] << ' ';
+        }
+        cout << endl;
     }
 };
 
 int stdteachsch::numofteachers = 0;
 
-
-int main() {
+int main()
+{
     Student students;
     stdteachsch tch;
     string choice;
     char arr1[3] = {'A', 'G', 'T'};
-    tch.setdata("Bijji", arr1, 3);
+    tch.setteaslots("Bijji", arr1, 3);
     char arr2[4] = {'Q','I','K','W'};
-    tch.setdata("Max",arr2,4);
+    tch.setteaslots("Max",arr2,4);
     char arr3[4]= {'Y','I','L','T'};
-    tch.setdata("Pratika",arr3,4);
+    tch.setteaslots("Pratika",arr3,4);
     char arr4[6]= {'B','E','H','M','O','Q'};
-    tch.setdata("Arun",arr4,6);
+    tch.setteaslots("Arun",arr4,6);
     char arr5[2]={'A','I'};
-    tch.setdata("Manjulika",arr5,2);
+    tch.setteaslots("Manjulika",arr5,2);
     do {
         system("cls");
         int option;
@@ -355,15 +334,14 @@ int main() {
         cout << "\n\t\t*** Student Management System ***";
         cout << "\n\t\t--------------------------------";
         cout << "\n\t\t 1. Add New Student" << endl;
-        cout << "\t\t 2. Display All Students" << endl;
-        cout << "\t\t 3. Search Student" << endl;
-        cout << "\t\t 4. Update Student" << endl;
-        cout << "\t\t 5. Delete Student" << endl;
-        cout << "\t\t 6. 9 pointers list" << endl;
-        cout << "\t\t 7. Register your free slots (only for teachers)" << endl;
-        cout << "\t\t 8. Check common slots with your teacher." << endl;
-        cout << "\t\t 9. Exit" << endl;
-        cout << "\t\t Enter Your Choice : ";
+        cout << "\n\t\t 2. Display All Students" << endl;
+        cout << "\n\t\t 3. Search Student" << endl;
+        cout << "\n\t\t 4. Update Student" << endl;
+        cout << "\n\t\t 5. 9 pointers list" << endl;
+        cout << "\n\t\t 6. Register your free slots (only for teachers)" << endl;
+        cout << "\n\t\t 7. Check common slots with your teacher." << endl;
+        cout << "\n\t\t 8. Exit" << endl;
+        cout << "\n\t\t Enter Your Choice : ";
         cin >> option;
 
         switch (option) {
@@ -371,17 +349,18 @@ int main() {
                 int rno, age;
                 string name;
                 float cgpa;
-                cout << "Enter Rollno :";
+                cout << "\t\tEnter Rollno :";
                 cin >> rno;
+
                 if((students.checkrollnum(rno))==1){
                     break;
                 }
-                cout << "Enter Name :";
+                cout << "\t\tEnter Name :";
                 cin.ignore();
                 getline(cin, name);
-                cout << "Enter Age :";
+                cout << "\t\tEnter Age :";
                 cin >> age;
-                cout << "Enter CGPA :";
+                cout << "\t\tEnter CGPA :";
                 cin >> cgpa;
                 students.addNewStudent(rno, name, age, cgpa);
                 break;
@@ -396,7 +375,7 @@ int main() {
                 int index = students.searchStudent(rollno);
                 if (index != -1) {
                     cout << "\t\t--------Student Found ------------" << endl;
-                    students.displayStudent(index);	
+                    students.displayStudent(index);    
                 } else {
                     cout << "\t\tStudent not found" << endl;
                 }
@@ -413,7 +392,7 @@ int main() {
                     cout << "\t\t 1. Update Rollno " << endl;
                     cout << "\t\t 2. Update Name " << endl;
                     cout << "\t\t 3. Update Age " << endl;
-                    cout << "\t\t 4.Update CGPA " << endl;
+                    cout << "\t\t 4. Update CGPA " << endl;
                     cout << "\t\t Enter Your Choice :";
                     cin >> choice;
                     students.updateStudent(index, choice);
@@ -422,41 +401,30 @@ int main() {
                 }
                 break;
             }
-            case 5: {
-                int rollno;
-                cout << "Enter Rollno to Delete :";
-                cin >> rollno;
-                int index = students.searchStudent(rollno);
-                if (index != -1) {
-                    students.deleteStudent(index);
-                } else {
-                    cout << "\t\tStudent not registered." << endl;
-                }
-                break;
-            }
-            case 6:{
+            case 5:{
                 students.display9plist();
                 break;
             }
-            case 7:{
-                tch.getslots();
+            case 6:{
+                tch.getteachslots();
                 break;
                 }
-            case 8:
+            case 7:
             {
                 tch.getstdslots();
                 tch.dispcommon();
                 break;
             }
         
-            case 9:
+            case 8:
                 exit(1);
-            default:
+            default:{
                 cout << "\t\tInvalid choice " << endl;
+            }
         }
-        cout << "\t\tDo You Want to Continue (Yes/No) ? :";
-        cin >> choice;
+    cout << "\t\tDo You Want to Continue (Yes/No) ? :";
+    cin >> choice;
     } while (choice[0] == 'y' || choice[0] == 'Y');
 
-    return 0;
+    return 0;
 }
